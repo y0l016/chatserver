7d602480ef00505d58049555aa4eaad1f265e9d5
viz <visuweshmuthukumar@gmail.com>
2019-08-14 18:42:54 +0530

    remove ip and get server address as cmdline argument

--- client.py
+++ client.py
@@ -1,14 +1,22 @@
+import argparse
+
 from socket import *
 
-HOST = '157.51.128.179:60364'
-PORT = 8000
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, 8000)) 
-while True:
-    message = input("Your Message: ")
-    s.send(bytes(message,"utf-8"))
-    print ("Awaiting reply")
-    reply = s.recv(1024) 
-    print ("Received ", repr(reply))
+if __main__ == "__name__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", metavar="", help="server's address")
 
-s.close()
+    if args.i:
+        tmp  = args.i.split(':')
+        HOST = tmp[0]
+        PORT = tmp[1]
+
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+
+        while 1:
+            message = input("Your Message: ")
+            s.send(bytes(message, "utf-8"))
+            print("Awaiting reply")
+            reply = s.recv(1024)
+            print("Received", repr(reply))
