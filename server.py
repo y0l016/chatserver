af2e0e16870a0fbf9ed5bf011bd4e61409bfb828
viz <visuweshmuthukumar@gmail.com>
2019-08-28 18:11:35 +0530

    server: implement the ban command
    to implement this command, data structures had to changed. namely the simple
    SOCKET_LIST had to be changed to a dict where the key is the addr of the socket
    and the value is the socket itself. also a OP_LIST has been added which contains
    all the addr of clients that can perform op actions such as ban itself.
    also the old send function is now called send_all.

--- server.py
+++ server.py
@@ -5,46 +5,118 @@ from select import select
 HOST        = ""
 PORT        = 9000
 SERV_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SOCKET_LIST = []
+SOCKET_LIST = {}
+BAN_LIST    = {}
+OP_LIST     = []
 
-def init():
-    SERV_SOCKET.bind((HOST, PORT))
-    SERV_SOCKET.listen()
-    SOCKET_LIST.append(SERV_SOCKET)
+# utils
 
-def main():
-    readable, _, _ = select(SOCKET_LIST, [], [], 0)
-
-    for sck in readable:
-        if sck == SERV_SOCKET:
-            # a new client has connected
-            # sockfd - socket object representing client
-            sockfd, _ = SERV_SOCKET.accept()
-            SOCKET_LIST.append(sockfd)
-        else:
-            # data is sent from the client
-            try:
-                data = sck.recv()
-                if data:
-                    # send data to all clients
-                    send(data, sck)
-                else:
-                    # client disconnected
-                    if sck in SOCKET_LIST: SOCKET_LIST.remove(sck)
-                    # TODO: send some other data
-            except:
-                pass
-
-def send(data, sent_from):
+def send_all(data, sent_from):
+    """
+    send data to everyone except sent_from.
+    """
     pass_scks = [SERV_SOCKET, sent_from]
-    for sck in SOCKET_LIST:
+    for sck in SOCKET_LIST.values():
         if sck not in pass_scks:
             try:
                 sck.send(data)
             except:
                 # broken client. prolly disconnected
+                addr, _ = sck.getpeername()
                 sck.close()
-                SOCKET_LIST.remove(sck)
+                SOCKET_LIST.remove(addr)
+
+def send_only(data, send_to):
+    """
+    send data only to send_to socket
+    returns True if succeded
+    """
+    try:
+        send_to.send(data)
+    except:
+        return False
+
+def gen_msg(msg):
+    """
+    generate message that will be sent by the server
+    """
+    return { "nick": "server", "msg": msg }
+
+# cmds
+def ban(clients):
+    msg = ""
+    nc = len(clients) - 1
+    for n, i in enumerate(clients):
+        addr, _ = i.getpeername()
+        if addr in BAN_LIST:
+            continue
+        BAN_LIST[addr] = i
+        SOCKET_LIST.pop(addr)
+        msg += f"server: banned {addr}"
+        if n != nc:
+            msg += "\n"
+    return gen_msg(msg)
+
+def handle_cmd(sock, data):
+    """
+    parse the command and handle the command.
+    arguments:
+    sock - sender's socket
+    data - sender's msg data
+    """
+    cmd = data.get("msg")[1:]
+    addr, _ = sock.getpeername()
+    if cmd.startswith("ban"):
+        if addr not in OP_LIST:
+            msg = gen_msg("you dont have permission to use this command")
+            ok = send_only(msg, sock)
+            if not ok:
+                sock.close()
+                SOCKET_LIST.remove(addr)
+                return gen_msg(f"{addr} disconnected")
+            else:
+                return False
+        return ban(msg.split()[1:])
+    elif cmd.startswith("disconnect"):
+        SOCKET_LIST.pop(addr)
+        # TODO: should we make the nick server?
+        # actually should we even send this message to people?
+        return gen_msg(f"{addr} disconnected")
+
+def init():
+    SERV_SOCKET.bind((HOST, PORT))
+    SERV_SOCKET.listen()
+    SOCKET_LIST[HOST] = SERV_SOCKET
+
+def main():
+    readable, _, _ = select(SOCKET_LIST.values(), [], [], 0)
+
+    for sck in readable:
+        if sck == SERV_SOCKET:
+            # a new client has connected
+            # sockfd - socket object representing client
+            sockfd, addr = SERV_SOCKET.accept()
+            SOCKET_LIST[addr] = sockfd
+        else:
+            # data is sent from the client
+            try:
+                data = sck.recv()
+                if data:
+                    if data.get("msg").startswith('/'):
+                        data = handle_cmd(sck, data)
+                        # client did something that they can't do
+                        if not data:
+                            continue
+                    # send data to all clients
+                    send_all(data, sck)
+                else:
+                    # client disconnected
+                    addr, _ = sck.getpeername()
+                    if addr in SOCKET_LIST:
+                        SOCKET_LIST.remove(addr)
+                    # TODO: send some other data
+            except:
+                pass
 
 if __name__ == "__main__":
     init()
